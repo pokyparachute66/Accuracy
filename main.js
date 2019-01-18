8cf46b9e20e22a4a1d2f9c597b8ac67d798435a5
pokyparachute66 <39124886+pokyparachute66@users.noreply.github.com>
2019-01-18 09:56:03 -0800

    second commit

--- main.js
+++ main.js
@@ -56,6 +56,36 @@ document.querySelector(".circle").addEventListener("mouseover", function(){
 
 
 
+
+
+
+
+/*
+
+Copyright (c) YEAR - Zac Holme - https://codepen.io/jaaclaine/pen/KmqEbq
+
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall 
+be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+*/
+
 var date = document.getElementsByClassName("day");
 
 for(i = 0; i < 50; i++){
@@ -110,4 +140,3 @@ function clickEvent(){
 function closeEvent(){
   document.getElementById("add").classList.remove("active");
 }
-
